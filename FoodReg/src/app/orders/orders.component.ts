639d00098bd663832b9148e0f754d14c8e3b851d
anand91 <91anandraj@gmail.com>
2019-09-25 12:12:19 -0400

    Data added to orders

--- FoodReg/src/app/orders/orders.component.ts
+++ FoodReg/src/app/orders/orders.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersComponent implements OnInit {
 
+  users = [{fname: 'Christina'},
+          {fname: 'Luke'},
+          {fname: 'Harsh'},
+          {fname: 'Levi'}]
   constructor() { }
 
   ngOnInit() {
